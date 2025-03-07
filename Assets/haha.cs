56e2844a2b94ca17062e712a4d63b0a35c348409
MrLuigiBean <prashanthsharma237@gmail.com>
2025-03-07 18:03:06 +0800

    mystery solved! :D ❤️
    https://docs.unity3d.com/6000.0/Documentation/ScriptReference/Rendering.GraphicsSettings-currentRenderPipeline.html

--- Assets/haha.cs
+++ Assets/haha.cs
@@ -1,5 +1,18 @@
+using TMPro;
 using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
 
 public class haha : MonoBehaviour
 {
+    [SerializeField] private Camera playerCamera;
+    [SerializeField] private TextMeshPro text;
+    public UniversalRendererData yes;
+
+    void Update()
+    {
+        // var yes = playerCamera.GetUniversalAdditionalCameraData().scriptableRenderer.supportedRenderingFeatures;
+        var yes = GraphicsSettings.currentRenderPipeline.name;
+        text.text = yes.ToString();
+    }
 }
